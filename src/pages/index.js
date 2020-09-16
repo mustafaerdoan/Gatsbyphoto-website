545b4a7e6ab1872bc729f42b897d6a773ef93399
mustafaerdoan <c.mustafaerdoann@hotmail.com>
2020-09-16 13:17:15 +0300

    ana sayfa fotoğraflar eklendi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
 import React from "react"
 
-
-
-
-const IndexPage = () => (
-  <div>
-
+import {graphql} from "gatsby"
+import ReactDOM from 'react-dom';
+import Img from "gatsby-image"
+const IndexPage = ({data}) => {
+ 
+  console.log(data)
+ 
+ return  <div>
+   
     <h1>Merhaba </h1>
+    {data.allFile.edges.map(({node}) =>{
+    return  <Img key={node.id} fluid={node.childImageSharp.fluid}></Img>
+    })}
   </div>
-)
+}
 
+
+export const pageQuery =graphql
+{
+    allFile(
+      filter: {absolutePath: {regex: "//photos//"}}) 
+ {
+    edges 
+     {
+      node 
+       {
+          id
+        childImageSharp 
+         {
+          fluid 
+           {
+             GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
 export default IndexPage
